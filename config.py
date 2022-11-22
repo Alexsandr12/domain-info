202d6cbbed4f7561de5545ac9e433fea716fe11a
Acer <magasov94@list.ru>
2022-11-22 19:04:13 +0400

    Изменен whois сервер, изменены данные подключения в бд в связи с добавлением .env

--- config.py
+++ config.py
@@ -1,11 +1,16 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv(".env")
+
 """web"""
 INDENT = 4
 ENSURE_ASCII = False
 
 """whois info"""
-WHOIS_TIMEOUT = 3
+WHOIS_TIMEOUT = 10
 PORT = 43
-WHOIS_SERVER = "whois.tcinet.ru"
+WHOIS_SERVER = "whois.reg.ru"
 
 """redis"""
 EXPIRED_RECORD = 3600
@@ -23,7 +28,7 @@ IP_ROOT_DNS = "193.232.128.6"
 LIMIT_DOMAINS = 5
 
 """sql"""
-HOST = "localhost"
-USER = "alexandr"
-PASSWORD = "1"
-DATABASE = "mysqltest"
+DSN = f"dbname={os.getenv('DATABASE')} " \
+      f"user={os.getenv('USER')} " \
+      f"password={os.getenv('PASSWORD')} " \
+      f"host={os.getenv('HOST')}"
